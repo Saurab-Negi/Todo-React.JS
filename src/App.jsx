08990a494da79ff6e921de7bcc56e3bc7c4cff0f
Saurab-Negi <saurabnegi17@gmail.com>
2024-08-09 11:47:17 +0530

    Code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Todo from "./Components/Todo/Todo";
+import Keys from "./Components/Keys/Keys"
 
 export default function App() {
   return (
     <>
       <Todo />
+      <Keys />
     </>
   )
 }
